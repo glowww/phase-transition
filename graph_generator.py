f8f3934a5993e302e2611bce8d4e1c5acc1b9f64
cirinatorres <cirinatorres@users.noreply.github.com>
2019-05-07 18:19:51 +0200

    Add files via upload

--- graph_generator.py
+++ graph_generator.py
@@ -4,12 +4,13 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.extended_barabasi_albert_graph(10, 2, 0.05, 0.05, seed=None)
-plt.subplot(121)
-nx.draw(G, with_labels=True, font_weight='bold')
-plt.subplot(122)
-nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-plt.show()
+# G = nx.extended_barabasi_albert_graph(10, 2, 0.05, 0.05, seed=None)
+G = nx.gnp_random_graph(10, 0.45, seed=None, directed=False)
+# plt.subplot(121)
+# nx.draw(G, with_labels=True, font_weight='bold')
+# plt.subplot(122)
+# nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
+# plt.show()
 
 
 # https://networkx.github.io/documentation/networkx-2.2/reference/readwrite/generated/networkx.readwrite.adjlist.write_adjlist.html
