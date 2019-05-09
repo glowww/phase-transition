7a19a5983fdb3abc724ccbfe5094b76b8347afee
cirinatorres <cirinatorres@users.noreply.github.com>
2019-05-10 00:44:22 +0200

    Add files via upload

--- percolation on gnp/graph_generator.py
+++ percolation on gnp/graph_generator.py
@@ -4,25 +4,22 @@ import sys, os, commands
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Usage: Usage: python graph_generator.py
+# Usage: Usage: python call.py
 # 				start probability=0.4, ending probability=0.6,
 # 				steps=0.01, repetions for graph=10, graph size=10
-# example: python graph_generator.py 0.4 0.6 0.01 10 10
-
-# start_p = int(float(sys.argv[1]) * 100)
-# end_p = int(float(sys.argv[2]) * 100) + 1
-# steps = int(float(sys.argv[3]) * 100)
-# rep_p = int(sys.argv[4]) + 1
-# size = int(sys.argv[5])
+# example: python call.py 0.4 0.6 0.01 50 10
 
 # start_p = int(float(0.5) * 100)
 # end_p = int(float(0.5) * 100) + 1
 # steps = int(float(0.01) * 100)
-rep_p = int(50) + 1
-size = 35
+# rep_p = int(1) + 1
+# size = 10
 
 # for i in range(start_p, end_p, steps):
 i = int(sys.argv[1])
+rep_p = int(sys.argv[2]) + 1
+size = int(sys.argv[3])
+
 open('list.txt', 'w').close()
 with open("list.txt", "a+") as file:
 	file.write(str(rep_p - 1) + " " + str(size) + '\n')
