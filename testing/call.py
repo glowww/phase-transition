2074fcf54fc47d1f2934749116946e340af3cc13
cirinatorres <cirinatorres@users.noreply.github.com>
2019-05-10 19:24:44 +0200

    Add files via upload

--- testing/call.py
+++ testing/call.py
@@ -1,8 +1,14 @@
 import os, sys
 
-start_p = int(float(0.00) * 100)
-end_p = int(float(1.0) * 100) + 1
-steps = int(float(0.01) * 100)
+start_p = int(float(sys.argv[1]) * 100)
+end_p = int(float(sys.argv[2]) * 100) + 1
+steps = int(float(sys.argv[3]) * 100)
+rep_p = int(sys.argv[4]) + 1
+size = int(sys.argv[5])
+
+# start_p = int(float(0.00) * 100)
+# end_p = int(float(1.0) * 100) + 1
+# steps = int(float(0.01) * 100)
 
 open('out.txt', 'w').close()
 for i in range(start_p, end_p, steps):
